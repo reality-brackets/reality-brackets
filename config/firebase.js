9529fadfd04b685ec27fb3c1a1b77c62a682e791
Morgan Crouch <morganloel@gmail.com>
2019-02-12 16:52:32 -0600

    setup

---
+++ config/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+import * as f from '../secrets';
+
+// Initialize Firebase
+export const config = {
+  apiKey: f.FIREBASE_API_KEY,
+  authDomain: f.FIREBASE_AUTH_DOMAIN,
+  databaseURL: f.FIREBASE_DATABASE_URL,
+  projectId: f.FIREBASE_PROJECT_ID,
+  storageBucket: f.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: f.FIREBASE_MESSAGING_SENDER_ID,
+};
+
+firebase.initializeApp(config);
+
+export const database = firebase.database();
+export const authorize = firebase.auth();
+// export const provider = firebase.auth.FacebookAuthProvider;
+export const storage = firebase.storage();
+export const provider = new firebase.auth.GoogleAuthProvider();
