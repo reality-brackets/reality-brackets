0c15904decd0e08d11e11e19c1e8ec7d2abfc4a8
Jingtin Tim Lin <lin.jingt@gmail.com>
2019-03-19 15:39:06 -0500

    typo

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stylesheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default class SignUp extends React.Component {
   constructor() {
