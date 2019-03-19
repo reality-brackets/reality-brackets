3dbe7c0cf22d89c29f2ba3ba04c6a4805c398576
Jingtin Tim Lin <lin.jingt@gmail.com>
2019-03-19 16:35:36 -0500

    added logout button/function

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Platform, Image, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { authorize } from '../config/firebase';
 
 export default class HomeScreen extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentUser: null
+      currentUser: null,
+      errorMessage: null
     };
   }
 
@@ -15,12 +16,22 @@ export default class HomeScreen extends React.Component {
     this.setState({currentUser})
   }
 
+  handleLogout = () => {
+    authorize.signOut()
+    .then(() => this.props.navigation.navigate('Auth'))
+    .catch(error => this.setState({errorMessage: error.message}))
+  }
+
   render() {
     const { currentUser } = this.state;
 
     return (
       <View style={styles.container}>
         <Text>Hi {currentUser && currentUser.email}!</Text>
+        {this.state.errorMessage && (
+          <Text style={{ color: 'red' }}>{this.state.errorMessage}</Text>
+        )}
+        <Button title="Logout" onPress={this.handleLogout} />
       </View>
     );
   }
